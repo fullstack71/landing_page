d8d5aab8674d522e0df2cdda1ff855092b3e8f26
Mahmudul Hasan <148671264+hasanfrombd@users.noreply.github.com>
2024-03-18 12:34:04 +0600

    swiper slider sample

--- js/landing_page_content.js
+++ js/landing_page_content.js
@@ -1,1 +1,19 @@
 document.getElementById('hero_video').playbackRate = 0.4;
+
+
+
+var swiper = new Swiper( '.swiper-container.two', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    effect: 'coverflow',
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflow: {
+        rotate: 0,
+        stretch: 100,
+        depth: 150,
+        modifier: 1.5,
+        slideShadows : false,
+    }
+} );
